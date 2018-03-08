428a32317cf0ef76b6451e35b3e6dc0b67dcb475
michael lin <mlininkl@gmail.com>
2018-03-08 09:33:22 +0800

    add numpy methods for mean, stddev, and score

--- nima_utils.py
+++ nima_utils.py
@@ -103,6 +103,42 @@ class NimaUtils(object):
     return tf.squeeze(rmse_scores)
 
   @staticmethod
+  def np_mu(y):
+    """from https://github.com/titu1994/neural-image-assessment/blob/master/utils/score_utils.py
+    accepts ratings as float, use for softmax
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """
+    si = np.arange(1,11,1, dtype=np.float32)
+    N = np.sum(y, axis=1)
+    mean = np.sum(y * si, axis=1)/N
+    return mean
+
+  @staticmethod
+  def np_sigma(y, mean=None):
+    """from https://github.com/titu1994/neural-image-assessment/blob/master/utils/score_utils.py
+    accepts ratings as float, use for softmax
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """
+    si = np.arange(1, 11, 1, dtype=np.float32)               # [1, n]
+    if not mean: mean = NimaUtils.np_mu(y)  # [m,]
+    mean = np.reshape(mean, [-1,1])    # [m,1]
+    N = np.sum(y, axis=1)
+    std = np.sqrt( np.sum(((si - mean) ** 2) * y, axis=1) /N  )
+    return std
+
+  @staticmethod
+  def np_score(y):
+    """ numpy calculation of [mean, std] from ratings as float, accepts softmax output
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """    
+    mean = NimaUtils.np_mu(y)
+    std = NimaUtils.np_sigma(y, mean)
+    return np.transpose([mean, std])      
+
+  @staticmethod
   def spearman_rank(y, y_hat):
     """returns the Spearman Ranked Correlation Coefficinet of the batch
       see: https://stackoverflow.com/questions/38487410/possible-to-use-rank-correlation-as-cost-function-in-tensorflow
@@ -186,13 +222,55 @@ class TestNimaUtils(object):
         x = []
         for k in range(n):
             for count in range(np.int(y[j,k])):
-                x.append(k+1)
+                x = np.append(x, [k+1])
+        # print(x)
         res.append(x)
-    return np.asarray(res)
-  
+    return res
+
   @staticmethod
-  def npscore(y):
-    """ numpy calculation of [mean, std] from expanded ratings """
+  def np_mean_score(y):
+    """from https://github.com/titu1994/neural-image-assessment/blob/master/utils/score_utils.py
+    accepts ratings as float, use for softmax
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """
+    si = np.arange(1,11,1, dtype=np.float32)
+    N = np.sum(y, axis=1)
+    mean = np.sum(y * si, axis=1)/N
+    return mean
+
+  @staticmethod
+  def np_std_score(y):
+    """from https://github.com/titu1994/neural-image-assessment/blob/master/utils/score_utils.py
+    accepts ratings as float, use for softmax
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """
+    si = np.arange(1, 11, 1, dtype=np.float32)               # [1, n]
+    mean = TestNimaUtils.np_mean_score(y)  # [m,]
+    mean = np.reshape(mean, [-1,1])    # [m,1]
+    N = np.sum(y, axis=1)
+    std = np.sqrt( np.sum(((si - mean) ** 2) * y, axis=1) /N  )
+    return std
+
+  @staticmethod
+  def np_score(y):
+    """ numpy calculation of [mean, std] from ratings as float, accepts softmax output
+    args:
+      y: array of shape [?,n], dtype=np.float
+    """    
+    mean = TestNimaUtils.np_mean_score(y)
+    std = TestNimaUtils.np_std_score(y)
+    return np.transpose([mean, std])    
+
+
+  @staticmethod
+  def np_score_int(y):
+    """ numpy calculation of [mean, std] from expanded ratings 
+    accepts ratings as int only
+    args:
+      y: array of shape [?,n], dtype=np.int
+    """
     m,n = np.shape(y)
     res = []
     x = TestNimaUtils.np_expand_ratings(y)
@@ -204,8 +282,11 @@ class TestNimaUtils(object):
   def test_score(y):
     """verify tf mu, sigma calculations match numpy"""
     tfscore = tf.Session().run( NimaUtils.score(y) )
-    np.testing.assert_allclose( tfscore, TestNimaUtils.npscore(y) )
+    np.testing.assert_allclose( tfscore, TestNimaUtils.np_score_int(y) )
     print("OK NimaUtils.score() matches manual numpy calculation")
+
+    np.testing.assert_allclose( TestNimaUtils.np_score_int(y), [TestNimaUtils.np_mean_score(y), TestNimaUtils.np_std_score(y)])
+    print("OK manual np_score_int() matches np_mean_score/np_std_score calculation")
       
   @staticmethod
   def test_emd_loss():
@@ -231,91 +312,91 @@ class TestNimaUtils(object):
 from models.research.slim.nets import vgg
 from tensorflow.contrib import slim
 
-# copied from nets.vgg.vgg_16 with slight modifications
-def nima_vgg_16(inputs,
-           num_classes=10,
-           is_training=True,
-           dropout_keep_prob=0.5,
-           dropout7_keep_prob=0.5,        # added kvarg to change value for dropout7 only
-           weight_decay=0.0005,
-           spatial_squeeze=True,
-           scope='vgg_16',
-           fc_conv_padding='VALID',
-           global_pool=False):
-  """Oxford Net VGG 16-Layers version D Example.
+# # copied from nets.vgg.vgg_16 with slight modifications
+# def nima_vgg_16(inputs,
+#            num_classes=10,
+#            is_training=True,
+#            dropout_keep_prob=0.5,
+#            dropout7_keep_prob=0.5,        # added kvarg to change value for dropout7 only
+#            weight_decay=0.0005,
+#            spatial_squeeze=True,
+#            scope='vgg_16',
+#            fc_conv_padding='VALID',
+#            global_pool=False):
+#   """Oxford Net VGG 16-Layers version D Example.
 
-  Note: All the fully_connected layers have been transformed to conv2d layers.
-        To use in classification mode, resize input to 224x224.
+#   Note: All the fully_connected layers have been transformed to conv2d layers.
+#         To use in classification mode, resize input to 224x224.
 
-  Args:
-    inputs: a tensor of size [batch_size, height, width, channels].
-    num_classes: number of predicted classes. If 0 or None, the logits layer is
-      omitted and the input features to the logits layer are returned instead.
-    is_training: whether or not the model is being trained.
-    dropout_keep_prob: the probability that activations are kept in the dropout
-      layers during training.
-    dropout7_keep_prob: ADDED to allow specification of value different from prior 
-      layers
-    spatial_squeeze: whether or not should squeeze the spatial dimensions of the
-      outputs. Useful to remove unnecessary dimensions for classification.
-    scope: Optional scope for the variables.
-    fc_conv_padding: the type of padding to use for the fully connected layer
-      that is implemented as a convolutional layer. Use 'SAME' padding if you
-      are applying the network in a fully convolutional manner and want to
-      get a prediction map downsampled by a factor of 32 as an output.
-      Otherwise, the output prediction map will be (input / 32) - 6 in case of
-      'VALID' padding.
-    global_pool: Optional boolean flag. If True, the input to the classification
-      layer is avgpooled to size 1x1, for any input size. (This is not part
-      of the original VGG architecture.)
+#   Args:
+#     inputs: a tensor of size [batch_size, height, width, channels].
+#     num_classes: number of predicted classes. If 0 or None, the logits layer is
+#       omitted and the input features to the logits layer are returned instead.
+#     is_training: whether or not the model is being trained.
+#     dropout_keep_prob: the probability that activations are kept in the dropout
+#       layers during training.
+#     dropout7_keep_prob: ADDED to allow specification of value different from prior 
+#       layers
+#     spatial_squeeze: whether or not should squeeze the spatial dimensions of the
+#       outputs. Useful to remove unnecessary dimensions for classification.
+#     scope: Optional scope for the variables.
+#     fc_conv_padding: the type of padding to use for the fully connected layer
+#       that is implemented as a convolutional layer. Use 'SAME' padding if you
+#       are applying the network in a fully convolutional manner and want to
+#       get a prediction map downsampled by a factor of 32 as an output.
+#       Otherwise, the output prediction map will be (input / 32) - 6 in case of
+#       'VALID' padding.
+#     global_pool: Optional boolean flag. If True, the input to the classification
+#       layer is avgpooled to size 1x1, for any input size. (This is not part
+#       of the original VGG architecture.)
 
-  Returns:
-    net: the output of the logits layer (if num_classes is a non-zero integer),
-      or the input to the logits layer (if num_classes is 0 or None).
-    end_points: a dict of tensors with intermediate activations.
-  """
-  # with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=0.0005)):
-  with tf.variable_scope(scope, 'vgg_16', [inputs]) as sc:
-    end_points_collection = sc.original_name_scope + '_end_points'
-    # Collect outputs for conv2d, fully_connected and max_pool2d.
-    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
-                        outputs_collections=end_points_collection):
-      net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
-      net = slim.max_pool2d(net, [2, 2], scope='pool1')
-      net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
-      net = slim.max_pool2d(net, [2, 2], scope='pool2')
-      net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
-      net = slim.max_pool2d(net, [2, 2], scope='pool3')
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
-      net = slim.max_pool2d(net, [2, 2], scope='pool4')
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
-      net = slim.max_pool2d(net, [2, 2], scope='pool5')
+#   Returns:
+#     net: the output of the logits layer (if num_classes is a non-zero integer),
+#       or the input to the logits layer (if num_classes is 0 or None).
+#     end_points: a dict of tensors with intermediate activations.
+#   """
+#   # with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=0.0005)):
+#   with tf.variable_scope(scope, 'vgg_16', [inputs]) as sc:
+#     end_points_collection = sc.original_name_scope + '_end_points'
+#     # Collect outputs for conv2d, fully_connected and max_pool2d.
+#     with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+#                         outputs_collections=end_points_collection):
+#       net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+#       net = slim.max_pool2d(net, [2, 2], scope='pool1')
+#       net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+#       net = slim.max_pool2d(net, [2, 2], scope='pool2')
+#       net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+#       net = slim.max_pool2d(net, [2, 2], scope='pool3')
+#       net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+#       net = slim.max_pool2d(net, [2, 2], scope='pool4')
+#       net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+#       net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
-      # Use conv2d instead of fully_connected layers.
-      net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
-      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                        scope='dropout6')
-      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-      # Convert end_points_collection into a end_point dict.
-      end_points = slim.utils.convert_collection_to_dict(end_points_collection)
-      if global_pool:
-        net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='global_pool')
-        end_points['global_pool'] = net
-      if num_classes:
-        net = slim.dropout(net, dropout7_keep_prob, is_training=is_training,
-                          scope='dropout7')
-        net = slim.conv2d(net, num_classes, [1, 1],
-                          activation_fn=None,
-                          normalizer_fn=None,
-                          scope='fc8')
-        if spatial_squeeze and num_classes is not None:
-          net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
-        end_points[sc.name + '/fc8'] = net
-      return net, end_points
+#       # Use conv2d instead of fully_connected layers.
+#       net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
+#       net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+#                         scope='dropout6')
+#       net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+#       # Convert end_points_collection into a end_point dict.
+#       end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+#       if global_pool:
+#         net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='global_pool')
+#         end_points['global_pool'] = net
+#       if num_classes:
+#         net = slim.dropout(net, dropout7_keep_prob, is_training=is_training,
+#                           scope='dropout7')
+#         net = slim.conv2d(net, num_classes, [1, 1],
+#                           activation_fn=None,
+#                           normalizer_fn=None,
+#                           scope='fc8')
+#         if spatial_squeeze and num_classes is not None:
+#           net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
+#         end_points[sc.name + '/fc8'] = net
+#       return net, end_points
 
-def scoped_nima_vgg_16(inputs, weight_decay=0.0005,  **kwargs):
-  with slim.arg_scope(vgg.vgg_arg_scope(weight_decay)):
-    return nima_vgg_16(inputs, **kwargs)
+# def scoped_nima_vgg_16(inputs, weight_decay=0.0005,  **kwargs):
+#   with slim.arg_scope(vgg.vgg_arg_scope(weight_decay)):
+#     return nima_vgg_16(inputs, **kwargs)
 
 
 def slim_learning_create_train_op_with_manual_grads( total_loss, 
